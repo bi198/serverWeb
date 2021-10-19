299a1f22af3f9910ec4b2c2f8fe80676c2b9723c
TrinhNguyen <bbgk198@gmail.com>
2021-10-19 09:11:31 +0700

    add file git ignore

--- data.js
+++ data.js
@@ -47,6 +47,18 @@ const data = {
       rating: 4.5,
       numReviews: 10,
     },
+     {
+      id: "5",
+      name: "Shirt 5",
+      image: "./imagies/img1.jpg",
+      price: 50,
+      brand: "Puma",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic",
+
+      rating: 4.5,
+      numReviews: 10,
+    },
   ],
 };
 export default data;
--- server.js
+++ server.js
@@ -7,5 +7,5 @@ app.get("/api/products", (req, res) => {
 });
 
 app.listen(5000, () => {
-  console.log("Server Started at https://localhost:5000");
+  console.log("Server Started at http://localhost:5000/");
 });
