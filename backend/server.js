69d202ed888d337f1fcc5d870a126365b0bcd02f
TrinhNguyen <bbgk198@gmail.com>
2021-10-25 18:42:53 +0700

    Manager State of product detail screen by React-Redux

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,22 @@
 import express from "express";
 import data from "./data";
 const app = express();
+// 1 PRODUCT
+// const Product_id = props.match.params.id;
+// const product = data.products.find((element) => element.id === Product_id);
 
+
+app.get("/api/products/:id", (req, res) => {
+  const productId = req.params.id;
+  const product = data.products.find((element) => element.id === productId);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ msg: "Product NOT FOUND !!" });
+  }
+});
+
+// ALL PRODUCT
 app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
