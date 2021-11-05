67567b48dc6e417d626a4a03b755f0b4f3f1ef0a
TrinhNguyen <bbgk198@gmail.com>
2021-11-05 22:03:03 +0700

    add change connect mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,32 @@
 import express from "express";
 import data from "./data";
+import dotenv from "dotenv";
+import config from "./config";
+import mongoose from "mongoose";
+import userRoute from "./routes/userRoute";
+
 const app = express();
-// 1 PRODUCT
-// const Product_id = props.match.params.id;
-// const product = data.products.find((element) => element.id === Product_id);
-
-
+dotenv.config();
+//  MONGODB COMPASS
+const mongodbUrl = config.MONGODB_URL;
+const mongodbOnlineUrl = config.MONGODB_ONLINE_URL;
+mongoose
+  .connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(console.log("Success connect to mongodb compass"))
+  .catch((error) => console.log(error.reason));
+//  MONGODB WEB
+// mongoose
+//   .connect(mongodbOnlineUrl, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//   }).then(console.log("Success connect to mongodb Online"))
+//   .catch((error) => console.log(error.reason));
+app.use("/api/users", userRoute);
 app.get("/api/products/:id", (req, res) => {
   const productId = req.params.id;
   const product = data.products.find((element) => element.id === productId);
@@ -22,6 +43,6 @@ app.get("/api/products", (req, res) => {
 });
 
 app.listen(5000, () => {
-  console.log("Server Started at http://localhost:5000/");
-  console.log("Server Data at http://localhost:5000/api/products");
+  console.log("Server started at http://localhost:5000");
+  console.log("Server Product at http://localhost:5000/api/products");
 });
